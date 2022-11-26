d2cd74452977ccfad22d2a58356f5a758ebb1b0e
Alex <alexsa-8@mail.ru>
2022-11-26 19:56:47 +0300

    калькулятор

---
+++ src/main/java/com/skypro/calkulator/CalkulatorApplication.java
@@ -0,0 +1,13 @@
+package com.skypro.calkulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalkulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalkulatorApplication.class, args);
+    }
+
+}
